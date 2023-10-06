edf61b9c08810affc376a3ddfd92f0684a604bb4
Maggie Barton <maggiebarton2@gmail.com>
2023-10-06 16:56:29 -0600

    spring boot REST project with JPA dependency

---
+++ SneakerStock/src/main/java/com/skilldistillery/sneakers/SneakerStockApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerStockApplication.class, args);
+	}
+
+}
---
+++ SneakerStock/src/test/java/com/skilldistillery/sneakers/SneakerStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sneakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
