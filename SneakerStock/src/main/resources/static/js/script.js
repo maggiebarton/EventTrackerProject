cf06cea037967ad5dc396b0c576cad2312005d43
Maggie Barton <maggiebarton2@gmail.com>
2023-10-15 19:59:56 -0600

    update sneaker working correctly, page reloads with update

--- SneakerStock/src/main/resources/static/js/script.js
+++ SneakerStock/src/main/resources/static/js/script.js
@@ -54,8 +54,11 @@ function init() {
 	document.addEventListener('click', function(e) {
 		let target = e.target.closest("#updateSneaker");
 		if (target) {
-			e.preventDefault();
 			console.log('update event listener working')
+			let sneakerId = e.target.parentElement.previousElementSibling.firstElementChild.id;
+			//let sneakerDetails = e.target.parentElement.previousElementSibling.firstElementChild;
+			console.log(sneakerId);
+			loadSingleSneaker(sneakerId);
 		}
 
 	})
@@ -67,6 +70,50 @@ function init() {
 			let sneakerId = e.target.parentElement.previousElementSibling.firstElementChild.id;
 			console.log(sneakerId);
 			deleteSneaker(sneakerId);
+		}
+
+	})
+
+	document.addEventListener('click', function(e) {
+		let target = e.target.closest("#update");
+		if (target) {
+			e.preventDefault();
+			console.log('update event listener working')
+			console.log(target.parentElement.id.value)
+			let updatedSneaker = {
+				id: target.parentElement.id.value,
+				collection: target.parentElement.collection.value,
+				name: target.parentElement.name.value,
+				size: target.parentElement.size.value,
+				retailPrice: target.parentElement.retailPrice.value,
+				releaseDate: target.parentElement.releaseDate.value,
+				acquisitionDate: target.parentElement.acquisitionDate.value,
+				colorway: target.parentElement.colorway.value,
+				box: target.parentElement.box.value,
+				imageURL: target.parentElement.imageURL.value,
+				brand: {
+					id: target.parentElement.brand.value
+				},
+				condition: {
+					id: target.parentElement.condition.value
+				}
+			}
+
+			updateSneaker(updatedSneaker);
+		}
+
+	})
+
+	document.addEventListener('click', function(e) {
+		let target = e.target.closest("#cancel");
+		if (target) {
+			e.preventDefault();
+			console.log('cancel event listener working')
+			let sneakerDiv = document.getElementById('allSneakers');
+			sneakerDiv.textContent = "";
+			let formDiv = document.getElementById('addSneakerFormDiv');
+			formDiv.textContent = "";
+			loadAllSneakers();
 		}
 
 	})
@@ -105,8 +152,10 @@ function updateSneaker(sneaker) {
 
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
-			if (xhr.status === 201) {
+			if (xhr.status === 200) {
 				let updatedSneaker = JSON.parse(xhr.responseText);
+				let sneakerDiv = document.getElementById('allSneakers');
+				sneakerDiv.textContent = "";
 				let formDiv = document.getElementById('addSneakerFormDiv');
 				formDiv.textContent = "";
 				loadAllSneakers();
@@ -157,6 +206,44 @@ function loadAllSneakers() {
 	xhr.send();
 }
 
+function loadSingleSneaker(sneakerId) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api/sneakers/' + sneakerId);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				let sneaker = JSON.parse(xhr.responseText);
+				createSneakerObject(sneaker);
+			} else {
+				//TODO
+			}
+		}
+	}
+	xhr.send();
+}
+
+function createSneakerObject(sneaker) {
+	let sneakerObj = {
+		id: sneaker.id,
+		collection: sneaker.collection,
+		name: sneaker.name,
+		size: sneaker.size,
+		retailPrice: sneaker.retailPrice,
+		releaseDate: sneaker.releaseDate,
+		acquisitionDate: sneaker.acquisitionDate,
+		colorway: sneaker.colorway,
+		box: sneaker.box,
+		imageURL: sneaker.imageURL,
+		brand: {
+			id: sneaker.brand.id
+		},
+		condition: {
+			id: sneaker.condition.id
+		}
+	}
+	displayUpdateSneakerForm(sneakerObj);
+}
+
 function loadAllBrands() {
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET', 'api/brands');
@@ -202,7 +289,7 @@ function displaySneakerList(sneakers) {
 		let thfilter = document.createElement('th');
 		thfilter.textContent = "Filter By";
 		let thblank = document.createElement('th');
-		thfilter.textContent = "";
+		thblank.textContent = "";
 
 		let tableBody = document.createElement('tbody');
 		div.append(table);
@@ -236,6 +323,7 @@ function displaySneakerList(sneakers) {
 
 			td1.appendChild(img);
 			let td2 = document.createElement('td');
+			td2.id = "sneakerDetails" + sneaker.id;
 			let details = "<div id=\"" + sneaker.id + "\">" +
 				sneaker.brand.name.toUpperCase() + " " + sneaker.collection.toUpperCase() + " '" + sneaker.name.toUpperCase() + "'" + "<br>" +
 				"Retail Price: $" + sneaker.retailPrice + "<br>" +
@@ -262,25 +350,6 @@ function displaySneakerList(sneakers) {
 
 function displayAddSneakerForm() {
 	let div = document.getElementById('addSneakerFormDiv');
-
-	/*	let h1 = document.createElement('h1');
-		h1.textContent = "Add Sneaker to Collection"
-		div.appendChild(h1);
-	
-		let form = document.createElement('form');
-		form.name = "addSneakerForm";
-		div.appendChild(form);
-	
-		let label1 = document.createElement('label');
-		label1.for = "collection";
-		label1.textContent = "Collection"
-		form.appendChild(label1);
-		let input1 = document.createElement('input');
-		input1.type = "text";
-		input1.name = "collection";
-		form.appendChild(input1);*/
-
-
 	let addForm =
 		`<h2>Add Sneaker to Collection</h2>
 			<form name="addSneakerForm">
@@ -340,4 +409,69 @@ function displayAddSneakerForm() {
 		</form>`;
 	div.innerHTML = addForm;
 
+}
+
+function displayUpdateSneakerForm(sneaker) {
+	console.log('update form');
+	let divId = "sneakerDetails" + sneaker.id;
+	let div = document.getElementById(divId);
+	let updateForm =
+		`<form name="addSneakerForm">
+				<input type="hidden" name="id" value="${sneaker.id}">
+				<label for="collection">Collection:</label><br>
+				<input type="text" name="collection" value="${sneaker.collection}"><br>
+	
+				<label for="name">Name:</label><br>
+				<input type="text" name="name" value="${sneaker.name}"><br>
+	
+				<label for="retailPrice">Retail Price:</label><br>
+				<input type="number" name="retailPrice" value="${sneaker.retailPrice}"><br>
+	
+				<label for="releaseDate">Release Date:</label><br>
+				<input type="date" name="releaseDate" value="${sneaker.releaseDate}"><br>
+	
+				<label for="acquisitionDate">Added to Collection:</label><br>
+				<input type="date" name="acquisitionDate" value="${sneaker.acquisitionDate}"><br>
+				
+				<label for="colorway">Colorway:</label><br>
+				<input type="text" name="colorway" value="${sneaker.colorway}"><br>
+				
+				<label for="size">Size:</label><br>
+				<input type="number" name="size" value="${sneaker.size}"><br>
+				
+				<label for="imageURL">Image URL:</label><br>
+				<input type="text" name="imageURL" value="${sneaker.imageURL}"><br>
+				
+				<label for="box">Has box?</label><br>
+				<input type="radio" id="true" name="box" value="true">
+				<label for="true">Yes</label>
+				<input type="radio" id="false" name="box" value="false">
+				<label for="false">No</label><br>
+				
+				<label for="brand">Brand:</label><br>
+				<select id="brand" name="brand">
+				 	<option value=1>Nike</option>
+  					<option value=2>Air Jordan</option>
+  					<option value=3>Adidas</option>
+ 					<option value=4>Reebok</option>
+ 					<option value=5>Heelys</option>
+ 					<option value=6>Adidas x Star Wars</option>
+				</select> <br>
+				
+				
+				
+				<label for="condition">Condition:</label><br>
+				<select id="condition" name="condition">
+					<option value=1>New</option>
+  					<option value=2>Used - Like New</option>
+  					<option value=3>Used - Good</option>
+ 					<option value=4>Used - Fair</option>
+ 					<option value=5>Used - Beat</option>
+				</select> <br>
+				
+				<button id="update" class="btn btn-secondary">Update</button>
+				<button id="cancel" class="btn btn-danger">Cancel</button>
+	
+		</form>`;
+	div.innerHTML = updateForm;
 }
