95c9b599d7dc5b672b75bdb423c181b30992f28a
Maggie Barton <maggiebarton2@gmail.com>
2023-10-20 15:52:57 -0600

    added crossorigin to controllers

--- SneakerStock/src/main/java/com/skilldistillery/sneakers/controllers/ConditionController.java
+++ SneakerStock/src/main/java/com/skilldistillery/sneakers/controllers/ConditionController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.sneakers.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.sneakers.entities.Condition;
 import com.skilldistillery.sneakers.services.ConditionService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class ConditionController {
