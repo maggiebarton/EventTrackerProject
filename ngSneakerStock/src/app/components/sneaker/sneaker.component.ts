7c84a015f7f8bc583f045cdec1bf6efc74cd6ad9
Maggie Barton <maggiebarton2@gmail.com>
2023-10-22 16:50:33 -0600

    minor bug fixes

--- ngSneakerStock/src/app/components/sneaker/sneaker.component.ts
+++ ngSneakerStock/src/app/components/sneaker/sneaker.component.ts
@@ -140,23 +140,7 @@ export class SneakerComponent implements OnInit{
   }
 
   open(content: any) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' })
 	}
 
-	private getDismissReason(reason: any): string {
-		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
 }
